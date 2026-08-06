3f5a51b41f4a2dda9a03a1bc68051fbc0015bc5f
Patrick Guenthard <me@patrickguenthard.ch>
2026-08-06 18:41:54 +0200

    calibration gui

---
+++ FSimCockpitApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace FSimCockpitApp.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ FSimCockpitApp/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace FSimCockpitApp.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
